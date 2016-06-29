81da16f70bd63ccb34c4892c0af9306e09ae96c7
Mark Lyck <mark.lyck@gmail.com>
2016-06-28 20:06:40 -0500

    Fixed hover color and cleaned up code

--- main.js
+++ main.js
@@ -1,41 +1,44 @@
-console.log('TABS!');
-
 var d = document;
-var tab1 = d.querySelector('#tab1');
-tab1.addEventListener('click', goToTab.bind(null, 1));
-var tab1content = d.querySelector('#tab1-content');
-var tab2 = d.querySelector('#tab2');
-tab2.addEventListener('click', goToTab.bind(null, 2));
-var tab2content = d.querySelector('#tab2-content');
-var tab3 = d.querySelector('#tab3');
-tab3.addEventListener('click', goToTab.bind(null, 3));
-var tab3content = d.querySelector('#tab3-content');
 
-window.location.hash = '#tab1';
-var currentHash = window.location.hash;
-var tabClicked = 1;
+var tabs = d.querySelectorAll('.tab');
+
+for(i=0; i < tabs.length; i++) {
+  tabs[i].addEventListener('click', clickHandler);
+}
+
+function clickHandler(evt) {
+  for(i=0; i < tabs.length; i++) {
+    tabs[i].classList.remove('current');
+    tabs[i].closest('li').style.border = '1px solid rgba(40,44,42,0.1)';
+  }
+  evt.target.classList.add('current');
+  evt.target.closest('li').style.borderBottom = 'none';
+  evt.target.closest('li').style.borderTop = '1px solid #2CC185';
+  location.hash = evt.target.hash;
+}
+
+var data = {
+  tab1: {
+    title: '1',
+    body: 'Tab 1 content'
+  },
+  tab2: {
+    title: '2',
+    body: 'Tab 2 content'
+  },
+  tab3: {
+    title: '3',
+    body: 'Tab 3 content'
+  }
+};
+
+renderContent('tab1');
 
 window.addEventListener("hashchange", function() {
-  if(tabClicked !== Number(window.location.hash.slice(-1))) {
-    goToTab(Number(window.location.hash.slice(-1)));
-  }
+  renderContent(window.location.hash.slice(1));
 });
 
-function goToTab(tab) {
-  tab1content.style.display = 'none';
-  tab2content.style.display = 'none';
-  tab3content.style.display = 'none';
-  if(tab === 1) {
-    tab1content.style.display = 'block';
-    tabClicked = 1;
-    window.location.hash = '#tab1';
-  } else if (tab === 2) {
-    tab2content.style.display = 'block';
-    tabClicked = 2;
-    window.location.hash = '#tab2';
-  } else {
-    tab3content.style.display = 'block';
-    tabClicked = 3;
-    window.location.hash = '#tab3';
-  }
+function renderContent(tab) {
+  d.querySelector('#content h1').textContent = data[tab].title;
+  d.querySelector('#content p').textContent = data[tab].body;
 }
